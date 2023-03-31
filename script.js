8a49a8b3321ee33a3d9218a2aa217b80e72b8c69
ventuscoder <shivankcode@gmail.com>
2023-03-31 16:50:24 +0530

    Write script

---
+++ script.js
@@ -0,0 +1,75 @@
+const colors = ['green', 'red', 'yellow', 'blue']
+let level = 0
+let sequence = []
+let userSequence = []
+let userCounter = 0
+let animationInterval = 150
+
+$(document).keydown(()=>{
+    startGame()
+})
+
+$('.start-btn').click(()=>{
+    startGame()
+})
+
+
+function addListeners() {
+    $('.btn').click((e)=>{
+        let inputClass = e.target.classList[1]
+        console.log(inputClass)
+        animatePress(inputClass)
+        userInput(inputClass)
+    })
+    
+}
+
+function startGame() {
+    if (!level) {
+        addLevel()
+        addListeners()
+    }
+}
+
+function addLevel() {
+    level++
+    $('#level-title').text('Level '+level)
+    $('.level').text(level)
+    $('.num').text('0')
+    sequence.push(colors[(Math.floor(Math.random()*colors.length))])
+    animateLevel()
+}
+
+function animateLevel() {
+    $('.'+sequence.at(-1)).addClass('pressed')
+    setTimeout(()=>{$('.'+sequence.at(-1)).removeClass('pressed')}, animationInterval)
+}
+
+function userInput(color) {
+    console.log(sequence,userCounter, userSequence)
+    if (sequence[userCounter]==color) {
+        userSequence.push(color)
+        userCounter++
+        console.log(sequence,userCounter, userSequence)
+        $('.num').text(userCounter)
+        if (level==userCounter) {
+            setTimeout(()=>{addLevel()}, 800)
+        }
+    } else {
+        endGame()
+    }
+}
+
+function animatePress(color) {
+    $('.'+color).addClass('pressed')
+    setTimeout(()=>{$('.'+color).removeClass('pressed')}, animationInterval)
+}
+
+function endGame() {
+    $('.btn').off('click')
+    userCounter = 0
+    userSequence = []
+    sequence = []
+    $('#level-title').text('Game ended at level '+level)
+    level = 0
+}
